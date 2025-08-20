96ba2030dfa3178eeee250dfb73de8335bf1332f
MrBodybag777 <162204405+MrBodybag777@users.noreply.github.com>
2025-08-20 13:46:13 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,34 @@
+
 namespace Store
 {	
 	
 	class Program
 	{
+		public static void reciptMaker(Dictionary<String, Double> products, List<String> cart,ref double balance)
+		{
+			double itemValue = 0;
+			double total = 0;
+			Console.WriteLine("\nRecipt~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			foreach(String item in cart){
+				itemValue = products[item];
+				Console.WriteLine("Product bought: {0} \nCost: {1}", item.PadLeft(31), Convert.ToString(itemValue).PadLeft(41));
+				Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+				total += itemValue;
+			}
+			Console.WriteLine("Total Amount:{0}", Convert.ToString(total).PadLeft(34));
+			Console.WriteLine("Remaining Blance: {0}", Convert.ToString(balance).PadLeft(29));
+			Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
+		}
 		public static void buyingProduct(Dictionary<String, Double> products, String product,ref Double balance)
 		{
 			balance -= products[product];
 		}
 		public static void shoppingLoop(Dictionary<String, Double> products,ref Double balance)
 		{	
+			List<String> cart = new List<String>();
 			for(;;){
-				List<String> cart = new List<String>();
 
 				Console.WriteLine("What item would you like to purches: ");
 				Console.WriteLine("Or checkout");
@@ -22,6 +39,7 @@ namespace Store
 					foreach (String product in cart){
 						buyingProduct(products, product,ref balance);
 					}
+					reciptMaker(products, cart,ref balance);
 					break;
 				}
 				if (products.ContainsKey(desiredProduct)){
@@ -38,13 +56,12 @@ namespace Store
 			products.Add("Apple", 5.99);
 			products.Add("Pizza", 9.99);
 			products.Add("Eggs", 4.59);
+			products.Add("Soda", 1.99);
 			foreach (KeyValuePair<String, Double>kvp in products){
 				Console.Write("--{0} {1}$--", kvp.Key, kvp.Value);
 			}
 			Console.Write("\n");
 			shoppingLoop(products,ref balance);
-			Console.WriteLine(balance);
-
 		}
 	}
 }
